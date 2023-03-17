45a2282097d0d6e38b9ef2b219dd9a0bb4b8e852
Duke4Coco <u531156798@byui.edu>
2023-03-17 11:11:17 +0200

    Made small modifications.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
 using Humanizer;
-Console.WriteLine("Hello, World!");
 
 static void HumanizeQuantities()
 {
-    Console.WriteLine("case".ToQuantity(0));
-    Console.WriteLine("case".ToQuantity(1));
-    Console.WriteLine("case".ToQuantity(5));
+    WriteLine("case".ToQuantity(0));
+    WriteLine("case".ToQuantity(1));
+    WriteLine("case".ToQuantity(5));
 }
 
 static void HumanizeDates()
 {
-    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
-    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
-    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+    WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    WriteLine(TimeSpan.FromDays(1).Humanize());
+    WriteLine(TimeSpan.FromDays(16).Humanize());
 }
+
+WriteLine("Quantities:");
+HumanizeQuantities();
+
+WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
 
